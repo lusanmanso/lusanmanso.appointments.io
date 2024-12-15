326857fe36d3984e8bb15d8bbcdba28434ac7ce8
Lucía <lucia.manso@live.u-tad.com>
2024-12-15 11:49:08 +0000

    corrections in inconsistencies of naming

--- DatabasePopulate.sql
+++ DatabasePopulate.sql
@@ -21,47 +21,46 @@ INSERT INTO Specialties (name, clinic_id) VALUES
 ('Otorhinolaryngology', 'D');
 
 -- Clinic A
--- Clinic A
 INSERT INTO Doctors (id, name, specialty_name, clinic_id) VALUES
-('DOCA1', 'Doctor A1', 'General Medicine', 'A'),
-('DOCA2', 'Doctor A2', 'General Medicine', 'A'),
-('DOCA3', 'Doctor A3', 'General Medicine', 'A'),
-('DOCA4', 'Doctor A4', 'General Medicine', 'A'),
-('DOCA5', 'Doctor A5', 'General Medicine', 'A'),
-('DOCA6', 'Doctor A6', 'General Medicine', 'A'),
-('DOCA7', 'Doctor A7', 'General Medicine', 'A'),
-('DOCA8', 'Doctor A8', 'General Medicine', 'A'),
-('DOCA9', 'Doctor A9', 'General Medicine', 'A'),
-('DOCA10', 'Doctor A10', 'General Medicine', 'A'), -- 10 doctors
+('DOCA1', 'Doctor A1', 'General Practice', 'A'),
+('DOCA2', 'Doctor A2', 'General Practice', 'A'),
+('DOCA3', 'Doctor A3', 'General Practice', 'A'),
+('DOCA4', 'Doctor A4', 'General Practice', 'A'),
+('DOCA5', 'Doctor A5', 'General Practice', 'A'),
+('DOCA6', 'Doctor A6', 'General Practice', 'A'),
+('DOCA7', 'Doctor A7', 'General Practice', 'A'),
+('DOCA8', 'Doctor A8', 'General Practice', 'A'),
+('DOCA9', 'Doctor A9', 'General Practice', 'A'),
+('DOCA10', 'Doctor A10', 'General Practice', 'A'),
 ('DOCA11', 'Doctor A11', 'Pediatrics', 'A'),
 ('DOCA12', 'Doctor A12', 'Pediatrics', 'A'),
 ('DOCA13', 'Doctor A13', 'Pediatrics', 'A'),
 ('DOCA14', 'Doctor A14', 'Pediatrics', 'A'),
-('DOCA15', 'Doctor A15', 'Pediatrics', 'A'), -- 5 doctors
-('DOCA16', 'Doctor A16', 'Cardiology', 'A'), -- 1 doctor
+('DOCA15', 'Doctor A15', 'Pediatrics', 'A'),
+('DOCA16', 'Doctor A16', 'Cardiology', 'A'),
 ('DOCA17', 'Doctor A17', 'Gynecology', 'A'),
-('DOCA18', 'Doctor A18', 'Gynecology', 'A'); -- 2 doctors
+('DOCA18', 'Doctor A18', 'Gynecology', 'A');
 
 -- Clinic B
 INSERT INTO Doctors (id, name, specialty_name, clinic_id) VALUES
-('DOCB1', 'Doctor B1', 'General Medicine', 'B'),
-('DOCB2', 'Doctor B2', 'General Medicine', 'B'),
-('DOCB3', 'Doctor B3', 'General Medicine', 'B'),
-('DOCB4', 'Doctor B4', 'General Medicine', 'B'),
-('DOCB5', 'Doctor B5', 'General Medicine', 'B'), -- 5 doctors
-('DOCB6', 'Doctor B6', 'Gynecology', 'B'), -- 1 doctor
-('DOCB7', 'Doctor B7', 'Hematology', 'B'); -- 1 doctor
+('DOCB1', 'Doctor B1', 'General Practice', 'B'),
+('DOCB2', 'Doctor B2', 'General Practice', 'B'),
+('DOCB3', 'Doctor B3', 'General Practice', 'B'),
+('DOCB4', 'Doctor B4', 'General Practice', 'B'),
+('DOCB5', 'Doctor B5', 'General Practice', 'B'),
+('DOCB6', 'Doctor B6', 'Gynecology', 'B'),
+('DOCB7', 'Doctor B7', 'Hematology', 'B');
 
 -- Clinic C
 INSERT INTO Doctors (id, name, specialty_name, clinic_id) VALUES
-('DOCC1', 'Doctor C1', 'Tropical Medicine', 'C'), -- 1 doctor
-('DOCC2', 'Doctor C2', 'Neurology', 'C'); -- 1 doctor
+('DOCC1', 'Doctor C1', 'Tropical Medicine', 'C'),
+('DOCC2', 'Doctor C2', 'Neurology', 'C');
 
 -- Clinic D
 INSERT INTO Doctors (id, name, specialty_name, clinic_id) VALUES
 ('DOCD1', 'Doctor D1', 'Ophthalmology', 'D'),
-('DOCD2', 'Doctor D2', 'Ophthalmology', 'D'), -- 2 doctors
+('DOCD2', 'Doctor D2', 'Ophthalmology', 'D'),
 ('DOCD3', 'Doctor D3', 'Oncology', 'D'),
 ('DOCD4', 'Doctor D4', 'Oncology', 'D'),
-('DOCD5', 'Doctor D5', 'Oncology', 'D'), -- 3 doctors
-('DOCD6', 'Doctor D6', 'Otorhinolaryngology', 'D'); -- 1 doctor
+('DOCD5', 'Doctor D5', 'Oncology', 'D'),
+('DOCD6', 'Doctor D6', 'Otorhinolaryngology', 'D');
